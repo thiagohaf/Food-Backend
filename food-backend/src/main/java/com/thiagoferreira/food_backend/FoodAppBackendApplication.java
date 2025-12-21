d2ee685811f519d797851786a2d0ec5918d97209
Thiago Ferreira <thiagohaf@yahoo.com.br>
2025-12-21 02:08:42 -0300

    Sets up initial project structure.
    Adds initial files and configurations for the food-backend project, including:
    - Maven wrapper for consistent builds
    - Gitignore configuration
    - Basic Spring Boot application setup
    - Project metadata and dependencies
    
    This commit establishes the foundation for the project.

---
+++ food-backend/src/main/java/com/thiagoferreira/food_backend/FoodAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoferreira.food_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppBackendApplication.class, args);
+	}
+
+}
---
+++ food-backend/src/test/java/com/thiagoferreira/food_backend/FoodAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoferreira.food_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
